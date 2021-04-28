b1e3d2a365b9aefd63f9ff927fbc33357f645cdf
sagatzt <sagatzt@gmail.com>
2021-04-28 08:15:01 +0200

    ok

--- modules/firebase/index.js
+++ modules/firebase/index.js
@@ -1,9 +1,5 @@
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
-const firebase = require("firebase/app");
-
-// Add the Firebase products that you want to use
-require("firebase/auth");
+const firebase = require("firebase/app")
+require("firebase/auth")
 
 const firebaseConfig = {
     apiKey: process.env.apiKey,
@@ -13,9 +9,9 @@ const firebaseConfig = {
     messagingSenderId: process.env.messagingSenderId,
     appId: process.env.appId,
     measurementId: process.envmeasurementId
-};
+}
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 let fbFunctions={}
 
@@ -23,16 +19,15 @@ fbFunctions.signup=(email,password)=>{
     return new Promise((resolved)=>{
         firebase.auth().createUserWithEmailAndPassword(email, password)
         .then(async (userCredential) => {
-          // Signed in
-          var user = userCredential.user;
+          var user = userCredential.user
           await userCredential.user.sendEmailVerification()
           resolved(user)
         })
         .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
+          var errorCode = error.code
+          var errorMessage = error.message
           resolved(errorCode,errorMessage)
-        });        
+        })      
     })
 }
 
